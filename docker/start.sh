5b4870ac8645838c1269b23b8f6cade44fb78032
hiddehs <hidde.schultze@gmail.com>
2020-06-05 12:10:02 +0200

    SQLCMD in web container, MSSQL db up and running, start.sh = 🚧

--- docker/start.sh
+++ docker/start.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
-until PGPASSWORD=mss-dev psql -h db -U postgres -lqt
-do
-	sleep 5;
-done;
+#until MSSQL_PASSWORD=mss-dev psql -h db -U postgres -lqt
+#do
+	sleep 500; # TODO remove debugging, for bashing directly into container to test
+#done;
 
 echo DB is up
 
-DB=`PGPASSWORD=mss-dev psql -h db -U postgres -lqt | cut -d \| -f 1 | grep mss`
+# TODO Check if DB mss exists
 
-if [ -z $DB ]; then
+#DB=`MSSQL_PASSWORD=mss-dev psql -h db -U sa -lqt | cut -d \| -f 1 | grep mss`
+
+#if [ -z $DB ]; then
     echo -n Create database...
-    echo create database mss2 | PGPASSWORD=mss-dev psql -h db -U postgres > /dev/null
+    
+    /opt/mssql-tools/bin/sqlcmd -S db_mssql -P "mss-nl-covid-19-dev" -U sa -Q "CREATE DATABASE mss" 
+   
+#    echo CREATE DATABASE mss2 | MSSQL_PASSWORD=mss-dev psql -h db -U sa > /dev/null
     echo done.
-    echo Running ProvisionDb...
-    cd /app/DataUtilities/ProvisionDb
-    dotnet run
-fi
+    
+    # TODO: PROVISIONDB IN CURRENT PROJECTVERSION
+    
+#    echo Running ProvisionDb...
+#    cd /app/DataUtilities/ProvisionDb
+#    dotnet run
+#fi
 
 cd /app/ServerStandAlone
 dotnet run
