5435ac50c819e4c1354a3efca4360936ef0b98cf
hiddehs <hidde.schultze@gmail.com>
2020-06-10 16:26:26 +0200

    Implemented InfectionConfirmationId frontend field

--- TestPortal/ClientApp/src/app/ICC/icc.component.ts
+++ TestPortal/ClientApp/src/app/ICC/icc.component.ts
@@ -7,15 +7,17 @@ import {Component} from '@angular/core';
 })
 export class ICCComponent {
   public icc_code = "123456"
-  public generateCode(){
-    const intervaller = setInterval(()=>{
-      this.icc_code = String(Math.floor(Math.random() * (999999 - 100000 + 1) ) + 100000);
+
+  public generateCode() {
+    const intervaller = setInterval(() => {
+      this.icc_code = String(Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000);
     }, 50)
     // TODO: Add API call
-    setTimeout(()=>{
+    setTimeout(() => {
       clearInterval(intervaller)
     }, 500)
   }
+
   public copyCodeToClipboard() {
     var textArea = document.createElement("textarea");
     textArea.value = this.icc_code;
@@ -33,5 +35,26 @@ export class ICCComponent {
     }
 
     document.body.removeChild(textArea);
+  }
+
+  public ICIdKeyPress($event: KeyboardEvent, index) {
+    if (index < 5) {
+      document.querySelector("#icIdWrapper .form-control:nth-child(" + (index + 2) + ")").focus()
+    } else {
+      setTimeout(()=>{
+        document.querySelector(".btn:last-child").focus()
+      })
+    }
+  }
+
+  public report() {
+    var ICId = Array.from(document.querySelectorAll("#icIdWrapper .form-control")).map(el => el.value).join("")
+    console.log(ICId);
+    setTimeout(()=>{
+      this.generateCode()
+      Array.from(document.querySelectorAll("#icIdWrapper .form-control")).forEach(el=>{
+        el.value = ""
+      })
+    }, 200)
   }
 }
