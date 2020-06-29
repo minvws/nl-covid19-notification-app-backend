9fb1eda0a467612b0b47d9d5deb25479cf53346d
Steve Kellaway <steve.kellaway@mefitihe.com>
2020-06-29 15:18:52 +0200

    Projects added.

---
+++ CdnPusherEngine2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CdnPusherEngine2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
