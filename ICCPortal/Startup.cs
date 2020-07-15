108f3bb6bf4c97f2886b4a3f77b37260acd64eee
hiddehs <hidde.schultze@gmail.com>
2020-07-15 13:28:20 +0200

    Added working @me endpoint and removed introspection

--- ICCPortal/Startup.cs
+++ ICCPortal/Startup.cs
@@ -3,7 +3,6 @@
 // SPDX-License-Identifier: EUPL-1.2
 
 using System;
-using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
