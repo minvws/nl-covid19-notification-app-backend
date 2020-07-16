71330b666a48afb839286a26f8a988a0af8ac794
Steve Kellaway <steve.kellaway@mefitihe.com>
2020-07-16 08:54:44 +0200

    ILogger for these 2.

--- CdnRegionSync/SyncFunctions.cs
+++ CdnRegionSync/SyncFunctions.cs
@@ -6,6 +6,7 @@ using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Mapping;
 using System;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace CdnRegionSync
 {
