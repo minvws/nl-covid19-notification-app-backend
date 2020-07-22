96ba9456ced476467f1d367a1c0c959c20a44bc3
Ryan Barrett <ryan@seldonplan.com>
2020-07-22 10:45:10 +0200

    Deploy pipeline follows new hosting structure.

--- package.bat
+++ package.bat
@@ -1,30 +1,28 @@
 @echo off
 
-cd publish
-
-REM Remove any confifugration files which have sneaked in to the packages
-del /S *.config
-del /S appsettings.json
-
 REM Create package directory. This is needed because CI server can only publish artifacts by directory
 mkdir packages
+
+REM Remove any confifugration files which have sneaked in to the packages
+cd publish
+del /S *.config
+del /S appsettings.json
 cd ..
 
 REM Zip websites
-7z a -tzip publish\packages\BatchJobsApi.zip publish\BatchJobsApi"
-7z a -tzip publish\packages\ContentApi.zip publish\ContentApi"
-7z a -tzip publish\packages\ICCBackend.zip publish\ICCBackend"
-7z a -tzip publish\packages\MobileAppApi.zip publish\MobileAppApi"
+7z a -tzip packages\Batch\Website.BatchJobsApi.zip publish\BatchJobsApi"
+7z a -tzip packages\App\Website.ContentApi.zip publish\ContentApi"
+7z a -tzip packages\App\Website.ICCBackend.zip publish\ICCBackend"
+7z a -tzip packages\App\Website.MobileAppApi.zip publish\MobileAppApi"
 
 REM Zip command-line
-7z a -tzip publish\packages\EksEngine.zip publish\EksEngine"
-7z a -tzip publish\packages\ManifestEngine.zip publish\ManifestEngine"
+7z a -tzip packages\Batch\CommandLine.EksEngine.zip publish\Batch.EksEngine"
+7z a -tzip packages\Batch\CommandLine.ManifestEngine.zip publish\Batch.ManifestEngine"
+
+REM Database
+7z a -tzip packages\SQL\SQL.DbProvision.zip publish\Tools\DbProvision"
 
 REM Zip tools
-7z a -tzip publish\packages\Tools\DbProvision.zip publish\Tools\DbProvision"
-7z a -tzip publish\packages\Tools\GenTeks.zip publish\Tools\GenTeks"
-
-REM Zip stand-alone server
-REM 7z a -tzip publish\packages\ServerStandAlone.zip publish\ServerStandAlone"
+7z a -tzip packages\Tools\CommandLine.GenTeks.zip publish\Tools\GenTeks"
 
 @echo on
