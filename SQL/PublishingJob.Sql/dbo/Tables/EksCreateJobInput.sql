39ed67636ac5a5b69fa9a3dae122502620bf1b8e
Rick van der Klugt <r.vd.klugt@minvws.nl>
2020-07-30 17:05:17 +0000

    Deleted SQL

--- SQL/PublishingJob.Sql/dbo/Tables/EksCreateJobInput.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[EksCreateJobInput] (
-    [Id]                    BIGINT          NOT NULL,
-    [Used]                  BIT             NOT NULL,
-    [KeyData]               VARBINARY (MAX) NOT NULL,
-    [RollingStartNumber]    INT             NOT NULL,
-    [RollingPeriod]         INT             NOT NULL,
-    [TransmissionRiskLevel] INT             NOT NULL
-);
