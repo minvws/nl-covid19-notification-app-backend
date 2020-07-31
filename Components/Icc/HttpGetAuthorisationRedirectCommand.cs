6baf9b013c8742f57aaa6f9bc78646ed30865ac3
hiddehs <hidde.schultze@gmail.com>
2020-07-31 16:07:46 +0200

    Added forwardedhost headername

--- Components/Icc/HttpGetAuthorisationRedirectCommand.cs
+++ Components/Icc/HttpGetAuthorisationRedirectCommand.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Icc.AuthHandlers;
 
 namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Icc
@@ -9,18 +10,21 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Icc
     {
         private readonly IIccPortalConfig _Configuration;
         private readonly IJwtService _JwtService;
+        private readonly ILogger<HttpGetAuthorisationRedirectCommand> _Logger;
 
-        public HttpGetAuthorisationRedirectCommand(IIccPortalConfig configuration, IJwtService jwtService)
+        public HttpGetAuthorisationRedirectCommand(IIccPortalConfig configuration, IJwtService jwtService, ILogger<HttpGetAuthorisationRedirectCommand> logger)
         {
             _Configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
             _JwtService = jwtService ?? throw new ArgumentNullException(nameof(jwtService));
+            _Logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public IActionResult Execute(HttpContext httpContext)
         {
             if (httpContext == null)
                 throw new ArgumentNullException(nameof(httpContext));
-
+                    
+            _Logger.LogInformation($"Executing Auth.Redirect on Host {httpContext.Request.Host.ToString()}");
             var jwtToken = _JwtService.Generate(httpContext.User);
 
             // temporary claim payload redirect solution for demo purposes
