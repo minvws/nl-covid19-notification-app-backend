572a5fdf77d8703651bad7d5a20fa3862ba10455
Steve Kellaway <steve.kellaway@mefitihe.com>
2020-08-04 13:26:26 +0200

    And the bat.

--- copy-CIBG-configs.bat
+++ copy-CIBG-configs.bat
@@ -1,7 +1,11 @@
-echo f|xcopy ..\s\publish\appsettings.release.json Artifacts\ContentApi\appsettings.json /Y
-echo f|xcopy ..\s\publish\appsettings.release.json Artifacts\ICCBackend\appsettings.json /Y
-echo f|xcopy ..\s\publish\appsettings.release.json Artifacts\MobileAppApi\appsettings.json /Y
+echo f|xcopy ..\s\publish\BatchJobsApi\appsettings.release.json Artifacts\BatchJobsApi\appsettings.json /Y
+echo f|xcopy ..\s\publish\ContentApi\appsettings.release.json Artifacts\ContentApi\appsettings.json /Y
+echo f|xcopy ..\s\publish\ICCBackend\appsettings.release.json Artifacts\ICCBackend\appsettings.json /Y
+echo f|xcopy ..\s\publish\MobileAppApi\appsettings.release.json Artifacts\MobileAppApi\appsettings.json /Y
 
-echo f|xcopy ..\s\appsettings.release.json Artifacts\EksEngine\appsettings.json /Y
-echo f|xcopy ..\s\appsettings.release.json Artifacts\ManifestEngine\appsettings.json /Y
-echo f|xcopy ..\s\appsettings.release.json Artifacts\DbProvision\appsettings.json /Y
+REM double check later
+echo f|xcopy ..\s\EksEngine\appsettings.release.json Artifacts\EksEngine\appsettings.json /Y
+REM double check later
+echo f|xcopy ..\s\ManifestEngine\appsettings.release.json Artifacts\ManifestEngine\appsettings.json /Y
+
+echo f|xcopy ..\s\DbProvision\appsettings.release.json Artifacts\DbProvision\appsettings.json /Y
