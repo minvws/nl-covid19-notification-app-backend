c3b1a0d629699e53d34fa49b3b15a8270f63f59d
Ryan Barrett <ryan@seldonplan.com>
2020-08-10 23:31:09 +0200

    Added missing copyright notice.

--- Components/WebApi/ResponsePaddingFilterAttribute.cs
+++ Components/WebApi/ResponsePaddingFilterAttribute.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.Logging;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Workflow.RegisterSecret;
