21ae8964c32ec9235683e5e7ab853b923348840f
Yorim <yorimchristiaanse@protonmail.com>
2020-08-24 16:05:20 +0200

    ManifestEngine was redundant; removed it.

--- SigTestFileCreator/Program.cs
+++ SigTestFileCreator/Program.cs
@@ -11,7 +11,6 @@ namespace SigTestFileCreator
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine.ContentFormatters;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine.FormatV1;
-    using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Mapping;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services.Signing;
 
@@ -37,7 +36,6 @@ namespace SigTestFileCreator
             services.AddSingleton<IEksHeaderInfoConfig, EksHeaderInfoConfig>();
             services.AddTransient<IEksContentFormatter, GeneratedProtobufEksContentFormatter>();
             services.AddTransient<IEksBuilder, EksBuilderV1>();
-            services.AddTransient<ZippedSignedContentFormatter>();
             services.NlSignerStartup();
             services.GaSignerStartup();
         }
--- SigTestFileCreator/SigTesterService.cs
+++ SigTestFileCreator/SigTesterService.cs
@@ -1,44 +1,36 @@
 ﻿namespace SigTestFileCreator
 {
     using System;
-    using System.Collections.Generic;
     using System.Threading.Tasks;
     using Microsoft.Extensions.Logging;
-    using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.EfDatabase.Entities;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine.FormatV1;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Framework;
     using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services;
     using System.IO;
-    using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Mapping;
 
     public sealed class SigTesterService
     {
         private readonly IEksBuilder _EksZipBuilder;
-        private readonly ZippedSignedContentFormatter _ManifestZipBuilder;
         private readonly IUtcDateTimeProvider _DateTimeProvider;
         private readonly ILogger _Logger;
 
         private byte[] _fileContents;
         private string _fileInputLocation;
         private string _eksFileOutputLocation;
-        private string _manifestFileOutputLocation;
 
         public SigTesterService(
             IEksBuilder eksZipBuilder,
-            ZippedSignedContentFormatter manifestZipBuilder,
             IUtcDateTimeProvider dateTimeProvider,
             ILogger<SigTesterService> logger
             )
         {
             _EksZipBuilder = eksZipBuilder ?? throw new ArgumentNullException(nameof(eksZipBuilder));
-            _ManifestZipBuilder = manifestZipBuilder ?? throw new ArgumentNullException(nameof(manifestZipBuilder));
             _DateTimeProvider = dateTimeProvider ?? throw new ArgumentNullException(nameof(dateTimeProvider));
             _Logger = logger ?? throw new ArgumentNullException(nameof(logger));
             
             _fileInputLocation = @"H:\test.txt";
             _eksFileOutputLocation = @"H:\testresult-eks.zip";
-            _manifestFileOutputLocation = @"H:\testresult-manifest.zip";
         }
 
         public async Task Execute(string[] args)
@@ -56,7 +48,6 @@
 
                 _fileInputLocation = CleanedInput;
                 _eksFileOutputLocation = FilePathWithoutExtension + "-eks" + ".Zip";
-                _manifestFileOutputLocation = FilePathWithoutExtension + "-manifest" + ".Zip";
             }
 
             if (Environment.UserInteractive && !WindowsIdentityStuff.CurrentUserIsAdministrator())
@@ -64,13 +55,9 @@
 
             LoadFile(_fileInputLocation);
             var eksZipOutput = await BuildEksOutput();
-            var manifestZipOutput = await BuildManifestOutput();
             
             _Logger.LogDebug("Saving EKS-engine resultfile.");
             ExportOutput(_eksFileOutputLocation, eksZipOutput);
-            
-            _Logger.LogDebug("Saving Manifest-engine resultfile.");
-            ExportOutput(_manifestFileOutputLocation, manifestZipOutput);
 
             _Logger.LogDebug("Key presence test complete.\nResults can be found in: {_fileOutputLocation}", _eksFileOutputLocation);
         }
@@ -114,12 +101,6 @@
             };
             
             return await _EksZipBuilder.BuildAsync(args);
-        }
-
-        private async Task<byte[]> BuildManifestOutput()
-        {
-            _Logger.LogDebug("Building Manifest-engine resultfile.");
-            return await _ManifestZipBuilder.SignedContentPacket(_fileContents);
         }
     }
 }
