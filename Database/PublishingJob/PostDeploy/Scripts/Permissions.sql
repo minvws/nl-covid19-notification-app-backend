85b733b6ea4db8d57c7fbe0e6c061bbcd41f6a38
d.dieben <d.dieben@minvws.nl>
2020-08-25 15:04:16 +0200

    Permissions according to table from Steve

--- Database/PublishingJob/PostDeploy/Scripts/Permissions.sql
+++ Database/PublishingJob/PostDeploy/Scripts/Permissions.sql
@@ -5,6 +5,17 @@ GRANT CONNECT TO [$(Domain)\$(ServiceAccount)];
 GRANT CONNECT TO [$(Domain)\$(ServiceAccountReport)];
 GRANT CONNECT TO [$(Domain)\$(Users)];
 
+--subroles
+GRANT CONNECT TO [$(Domain)\$(MobileAppAPI)];
+GRANT CONNECT TO [$(Domain)\$(IccPortal)];
+GRANT CONNECT TO [$(Domain)\$(EksEngine)];
+GRANT CONNECT TO [$(Domain)\$(ManifestEngine)];
+GRANT CONNECT TO [$(Domain)\$(ContentAPI)];
+GRANT CONNECT TO [$(Domain)\$(CleanupJob)];
+GRANT CONNECT TO [$(Domain)\$(ManagementPortal)];
+GRANT CONNECT TO [$(Domain)\$(DbProvision)];
+GRANT CONNECT TO [$(Domain)\$(GenTeks)];
+
 GRANT DELETE  ON SCHEMA::[dbo] TO [Dbr_Service];
 GRANT EXECUTE ON SCHEMA::[dbo] TO [Dbr_Service];
 GRANT INSERT  ON SCHEMA::[dbo] TO [Dbr_Service];
@@ -12,6 +23,19 @@ GRANT SELECT  ON SCHEMA::[dbo] TO [Dbr_Service];
 GRANT UPDATE  ON SCHEMA::[dbo] TO [Dbr_Service];
 
 GRANT SELECT  ON SCHEMA::[dbo] TO [Dbr_Funcbeheerders];
+
+--specific permissions for Content here..
+--[Dbr_Service_EksEngine]
+GRANT SELECT ON [dbo].[EksCreateJobInput] TO [Dbr_Service_EksEngine];
+GRANT INSERT ON [dbo].[EksCreateJobInput] TO [Dbr_Service_EksEngine];
+GRANT UPDATE ON [dbo].[EksCreateJobInput] TO [Dbr_Service_EksEngine];
+GRANT DELETE ON [dbo].[EksCreateJobInput] TO [Dbr_Service_EksEngine];
+
+GRANT SELECT ON [dbo].[EksCreateJobOutput] TO [Dbr_Service_EksEngine];
+GRANT INSERT ON [dbo].[EksCreateJobOutput] TO [Dbr_Service_EksEngine];
+GRANT UPDATE ON [dbo].[EksCreateJobOutput] TO [Dbr_Service_EksEngine];
+GRANT DELETE ON [dbo].[EksCreateJobOutput] TO [Dbr_Service_EksEngine];
+--end 
 
 GRANT VIEW ANY COLUMN ENCRYPTION KEY DEFINITION TO PUBLIC;
 GRANT VIEW ANY COLUMN MASTER KEY DEFINITION TO PUBLIC;
--- Database/WorkFlow/PostDeploy/Scripts/Permissions.sql
+++ Database/WorkFlow/PostDeploy/Scripts/Permissions.sql
@@ -5,6 +5,17 @@ GRANT CONNECT TO [$(Domain)\$(ServiceAccount)];
 GRANT CONNECT TO [$(Domain)\$(ServiceAccountReport)];
 GRANT CONNECT TO [$(Domain)\$(Users)];
 
+--subroles
+GRANT CONNECT TO [$(Domain)\$(MobileAppAPI)];
+GRANT CONNECT TO [$(Domain)\$(IccPortal)];
+GRANT CONNECT TO [$(Domain)\$(EksEngine)];
+GRANT CONNECT TO [$(Domain)\$(ManifestEngine)];
+GRANT CONNECT TO [$(Domain)\$(ContentAPI)];
+GRANT CONNECT TO [$(Domain)\$(CleanupJob)];
+GRANT CONNECT TO [$(Domain)\$(ManagementPortal)];
+GRANT CONNECT TO [$(Domain)\$(DbProvision)];
+GRANT CONNECT TO [$(Domain)\$(GenTeks)];
+
 GRANT DELETE  ON SCHEMA::[dbo] TO [Dbr_Service];
 GRANT EXECUTE ON SCHEMA::[dbo] TO [Dbr_Service];
 GRANT INSERT  ON SCHEMA::[dbo] TO [Dbr_Service];
@@ -12,6 +23,46 @@ GRANT SELECT  ON SCHEMA::[dbo] TO [Dbr_Service];
 GRANT UPDATE  ON SCHEMA::[dbo] TO [Dbr_Service];
 
 GRANT SELECT  ON SCHEMA::[dbo] TO [Dbr_Funcbeheerders];
+
+--specific permissions for Content here..
+--[Dbr_Service_MobileAppAPI]
+GRANT DELETE ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_MobileAppAPI];
+GRANT INSERT ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_MobileAppAPI];
+GRANT SELECT ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_MobileAppAPI];
+GRANT UPDATE ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_MobileAppAPI];
+
+GRANT UPDATE ON [dbo].[TemporaryExposureKeys] TO [Dbr_Service_MobileAppAPI];
+
+--[Dbr_Service_IccPortal]
+GRANT SELECT ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_IccPortal];
+GRANT UPDATE ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_IccPortal];
+
+GRANT SELECT ON [dbo].[TemporaryExposureKeys] TO [Dbr_Service_IccPortal];
+GRANT UPDATE ON [dbo].[TemporaryExposureKeys] TO [Dbr_Service_IccPortal];
+
+--[Dbr_Service_EksEngine] 
+GRANT SELECT ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_EksEngine];
+GRANT UPDATE ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_EksEngine];
+
+GRANT SELECT ON [dbo].[TemporaryExposureKeys] TO Dbr_Service_EksEngine;
+GRANT UPDATE ON [dbo].[TemporaryExposureKeys] TO Dbr_Service_EksEngine;
+
+--[Dbr_Service_CleanupJob] 
+GRANT SELECT ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_CleanupJob];
+GRANT DELETE ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_CleanupJob];
+
+GRANT SELECT ON [dbo].[TemporaryExposureKeys] TO [Dbr_Service_CleanupJob];
+GRANT DELETE ON [dbo].[TemporaryExposureKeys] TO [Dbr_Service_CleanupJob];
+
+--[Dbr_Service_ManagementPortal] 
+GRANT SELECT ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_ManagementPortal];
+
+GRANT SELECT ON [dbo].[TemporaryExposureKeys] TO [Dbr_Service_ManagementPortal];
+
+--[Dbr_Service_GenTeks] 
+GRANT INSERT ON [dbo].[TekReleaseWorkflowState] TO [Dbr_Service_GenTeks];
+GRANT INSERT ON [dbo].[TemporaryExposureKeys] TO [Dbr_Service_GenTeks];
+--end 
 
 GRANT VIEW ANY COLUMN ENCRYPTION KEY DEFINITION TO PUBLIC;
 GRANT VIEW ANY COLUMN MASTER KEY DEFINITION TO PUBLIC;
