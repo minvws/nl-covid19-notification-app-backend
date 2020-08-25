9e364516ef90ffe4275d07451a285eaf3fec8686
Steve Kellaway <steve.kellaway@mefitihe.com>
2020-08-25 16:44:17 +0200

    Write sig and content to byte files

---
+++ Components.Tests/ExposureKeySets/ExposureKeySetSigExtractorTests.cs
@@ -0,0 +1,37 @@
+﻿// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine.ContentFormatters;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine.FormatV1;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services.Signing.Providers;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services.Signing.Signers;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Workflow;
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Tests.ExposureKeySets
+{
+    [TestClass]
+    public class ExposureKeySetSigExtractorTests
+    {
+
+        [TestMethod]
+        public void Yank()
+        {
+            const string eks = "1a082a5e05cd791ef7fbabdf3b653d3d9363d1dfb791d026e81db519500b090c";
+
+            var folder = Path.GetDirectoryName(NCrunch.Framework.NCrunchEnvironment.GetOriginalProjectPath());
+            var filename = Path.Combine(folder, eks);
+            var bytes = new SigReader().Read(filename);
+            using var output = File.Create(@"D:\sig.bin", 1024, FileOptions.None);
+            output.Write(bytes);
+        }
+    }
+}
---
+++ Components/ExposureKeySetsEngine/ContentFormatters/EksParser.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.IO.Compression;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.GeneratedGaenFormat;
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine.ContentFormatters
+{
+    public class EksParser
+    {
+        private const string ContentEntryName = "export.bin";
+        private const string GaenSignaturesEntryName = "export.sig";
+        public byte[] ReadGaenSig(string filename)
+        {
+            using var zipStream = File.Open(filename, FileMode.Open);
+            using var zip = new ZipArchive(zipStream);
+            var entry = zip.GetEntry(GaenSignaturesEntryName);
+            using var entryStream = entry.Open();
+            var list = TEKSignatureList.Parser.ParseFrom(entryStream);
+            return list.Signatures[0].Signature.ToByteArray();
+        }
+        public byte[] ReadContent(string filename)
+        {
+            using var zipStream = File.Open(filename, FileMode.Open);
+            using var zip = new ZipArchive(zipStream);
+            var entry = zip.GetEntry(ContentEntryName);
+            using var result = new MemoryStream();
+            using var entryStream = entry.Open();
+            entryStream.CopyTo(result);
+            return result.ToArray();
+        }
+   }
+}
