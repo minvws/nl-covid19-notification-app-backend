6e53fb2d2f87341e7830d2ffe59bb845ea7d8337
hiddehs <hidde.schultze@gmail.com>
2020-08-26 08:52:01 +0200

    Add AuthCodeServiceTests

--- Components.Tests/Icc/AuthCodeServiceTests.cs
+++ Components.Tests/Icc/AuthCodeServiceTests.cs
@@ -2,7 +2,12 @@
 // Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
 // SPDX-License-Identifier: EUPL-1.2
 
+using System.Security.Claims;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Icc.AuthHandlers;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.WebApi;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Workflow.RegisterSecret;
+using TheIdentityHub.AspNetCore.Authentication;
 
 namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Tests.Icc
 {
@@ -10,41 +15,50 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Tests.Icc
     public class AuthCodeServiceTests
     {
         //
-        // private 
-        //
-        //
-        // private ITheIdentityHubService _TheIdentityHubService;
-        // private IOptionsMonitor<TheIdentityHubOptions> _Options;
-        // private WireMockServer _Server;
-        //
-        // [TestInitialize]
-        // public void Initialize()
-        // {
-        //     var logger = new TestLogger<TheIdentityHubService>();
-        //     _Server = WireMockServer.Start();
-        //     
-        //     if (_Server.Urls.Length < 1)
-        //     {
-        //         Assert.Fail("WireMockServer not started correctly");
-        //         return;
-        //     }
-        //
-        //     var serviceCollection = new ServiceCollection();
-        //     serviceCollection.AddCustomOptions(TheIdentityHubDefaults.AuthenticationScheme, options =>
-        //     {
-        //         options.TheIdentityHubUrl = new Uri(_Server.Urls[0]);
-        //         options.Tenant = "ggdghornl_test";
-        //         options.ClientId = "0";
-        //         options.ClientSecret = "supersecret";
-        //         options.CallbackPath = "/signin-identityhub";
-        //         options.Backchannel = new HttpClient();
-        //     });
-        //     var builder = serviceCollection.BuildServiceProvider();
-        //     _Options = builder.GetService<IOptionsMonitor<TheIdentityHubOptions>>();
-        //
-        //     _TheIdentityHubService = new TheIdentityHubService(_Options, logger);
-        // }
-        //
-        //
+        private IAuthCodeService _AuthCodeService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            var cryptoRandomPaddingGenerator = new CryptoRandomPaddingGenerator(new StandardRandomNumberGenerator());
+            _AuthCodeService = new AuthCodeService(cryptoRandomPaddingGenerator);
+        }
+
+        [TestMethod]
+        public void ValidateIfGeneratedAuthCodeIsStored()
+        {
+            var authCode = _AuthCodeService.Generate(new ClaimsPrincipal());
+
+            var result = _AuthCodeService.TryGetClaims(authCode, out _);
+
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void ValidateIfClaimsPrincipalIsStored()
+        {
+            var identity = new ClaimsIdentity(null, TheIdentityHubDefaults.AuthenticationScheme);
+            var expectedClaimsPrincipal = new ClaimsPrincipal(identity);
+
+            var authCode = _AuthCodeService.Generate(expectedClaimsPrincipal);
+
+            var successful = _AuthCodeService.TryGetClaims(authCode, out var outputClaims);
+
+            Assert.IsTrue(successful);
+            Assert.AreEqual(expectedClaimsPrincipal, outputClaims);
+        }
+
+        [TestMethod]
+        public void ValidateIfAuthCodeIsRevokedCorrectly()
+        {
+            var authCode = _AuthCodeService.Generate(new ClaimsPrincipal());
+
+            _AuthCodeService.RevokeAuthCode(authCode);
+
+            var result = _AuthCodeService.TryGetClaims(authCode, out _);
+
+            Assert.IsFalse(result);
+        }
     }
 }
