ff4a14e935a362fb02f147e61b9e156925eec175
hiddehs <hidde.schultze@gmail.com>
2020-08-28 12:18:33 +0200

    logging msg

--- Components/Icc/TestJwtGeneratorService.cs
+++ Components/Icc/TestJwtGeneratorService.cs
@@ -24,7 +24,7 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Icc
             _Logger = logger ?? throw new ArgumentNullException(nameof(logger));
         
         
-            _Logger.LogInformation("TestJwtGeneratorService Singleton constructed, generating test accesstoken now...");
+            _Logger.LogInformation("TestJwtGeneratorService Singleton constructed, generating test JWT now...");
             var testJwtData = new Dictionary<string, object> {{"access_token", "test_access_token"}, {"id", "0"}};
         
             var expiry = new StandardUtcDateTimeProvider().Now().AddDays(7).ToUnixTimeU64();
