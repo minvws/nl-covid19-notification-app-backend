8aa624ca9b301f48d9f489fb1471260fdc66fd4f
Marc Vantveer <marc.vantveer@polteq.com>
2020-09-16 16:14:15 +0200

    update payload folder location in testsig function

--- Testautomation/util/icc_bearer_token_data.js
+++ Testautomation/util/icc_bearer_token_data.js
@@ -1,6 +1,6 @@
 // Set bearer token to login to ICC portal
 
 const bearer =
-  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDAxNjkxODIsImlkIjoiNDUzNjQ0MDYyNDUyNTk4Njk4OCIsImFjY2Vzc190b2tlbiI6ImYzQlpzY2g3MUtrVFdTUzdmeDFyZ2ciLCJuYW1lIjoiTWFyYyB2YW4g4oCZdCBWZWVyIn0.OcIBpsh0Qh9ECj1VHU8ScXOLv3DEGEChqgXs0m9xlJ4";
+  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDAyNjgxMjgsImlkIjoiNDUzNjQ0MDYyNDUyNTk4Njk4OCIsImFjY2Vzc190b2tlbiI6Ik5VRlZYa0NLTWxfOWpSdGh4aHVJa0EiLCJuYW1lIjoiTWFyYyB2YW4g4oCZdCBWZWVyIn0.mkAGlwR8K2s5v_QDt2jAU_ldv_LftharG9vdi-R-D6w";
 
 module.exports = bearer;
