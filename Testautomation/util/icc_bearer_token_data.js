aa551fb1ced898e8ca92684dd71c8a8ca0c871ae
Marc Vantveer <marc.vantveer@polteq.com>
2020-09-18 11:03:46 +0200

    updated example scenario's

--- Testautomation/util/icc_bearer_token_data.js
+++ Testautomation/util/icc_bearer_token_data.js
@@ -1,6 +1,6 @@
 // Set bearer token to login to ICC portal
 
 const bearer =
-  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDAyNjgxMjgsImlkIjoiNDUzNjQ0MDYyNDUyNTk4Njk4OCIsImFjY2Vzc190b2tlbiI6Ik5VRlZYa0NLTWxfOWpSdGh4aHVJa0EiLCJuYW1lIjoiTWFyYyB2YW4g4oCZdCBWZWVyIn0.mkAGlwR8K2s5v_QDt2jAU_ldv_LftharG9vdi-R-D6w";
+  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDA0MTg3NjUsImlkIjoiNDUzNjQ0MDYyNDUyNTk4Njk4OCIsImFjY2Vzc190b2tlbiI6InFoYjQyWTZNMHZzLTA0Z0pCOVVCZHciLCJuYW1lIjoiTWFyYyB2YW4g4oCZdCBWZWVyIn0.tvikeuoTqLYl7YL1_6ETb8_yMXxK2hZKJbNuvXaLDug";
 
 module.exports = bearer;
