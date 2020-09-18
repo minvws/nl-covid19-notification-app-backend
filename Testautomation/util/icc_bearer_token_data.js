319050635ca5e279a64a8e319a7aa1e6ee45ed5f
Ryan Barrett <ryan@seldonplan.com>
2020-09-18 10:59:19 +0200

    Added a configuration file with a token for the CI server.

--- Testautomation/util/icc_bearer_token_data.js
+++ Testautomation/util/icc_bearer_token_data.js
@@ -1,6 +1,7 @@
 // Set bearer token to login to ICC portal
 
-const bearer =
-  "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MDAyNjgxMjgsImlkIjoiNDUzNjQ0MDYyNDUyNTk4Njk4OCIsImFjY2Vzc190b2tlbiI6Ik5VRlZYa0NLTWxfOWpSdGh4aHVJa0EiLCJuYW1lIjoiTWFyYyB2YW4g4oCZdCBWZWVyIn0.mkAGlwR8K2s5v_QDt2jAU_ldv_LftharG9vdi-R-D6w";
+require('../config.json')
+
+const bearer = portal.auth.token;
 
 module.exports = bearer;
