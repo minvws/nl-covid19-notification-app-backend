8a73933469f3c0731672c373853096016e092373
Yorim <yorimchristiaanse@protonmail.com>
2020-09-25 18:19:53 +0200

    Initial sketch

---
+++ Components/WebApi/DecoyTimeGeneratorAttribute.cs
@@ -0,0 +1,44 @@
+﻿// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.WebApi
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.Logging;
+    using System.Threading.Tasks;
+    using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Workflow.RegisterSecret;
+    using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Workflow.DecoyKeys;
+
+    // NOTE: DO not apply this attribute directly, apply DecoyTimeGeneratorAttributeFactory
+    public class DecoyTimeGeneratorAttribute : ActionFilterAttribute
+    {
+        private readonly ILogger _Logger;
+        private readonly IRandomNumberGenerator _RandomNumberGenerator;
+        private readonly IDecoyKeysConfig _Config;
+
+        public DecoyTimeGeneratorAttribute(
+            ILogger<DecoyTimeGeneratorAttribute> logger,
+            IRandomNumberGenerator randomNumberGenerator,
+            IDecoyKeysConfig config)
+        {
+            _Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _RandomNumberGenerator = randomNumberGenerator ?? throw new ArgumentNullException(nameof(randomNumberGenerator));
+            _Config = config ?? throw new ArgumentNullException(nameof(config));
+        }
+
+        //Todo: check whether to execute before result is given.
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            base.OnActionExecuting(context);
+            var result = next();
+
+            var delayInMilliseconds = _RandomNumberGenerator.Next(_Config.MinimumDelayInMilliseconds, _Config.MaximumDelayInMilliseconds);
+            _Logger.LogDebug("Delaying for {DelayInMilliseconds} seconds", delayInMilliseconds);
+
+            return new Task(() => Task.Delay(delayInMilliseconds));
+        }
+    }
+}
---
+++ Components/WebApi/DecoyTimeGeneratorAttributeFactory.cs
@@ -0,0 +1,22 @@
+﻿// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.WebApi
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc.Filters;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class DecoyTimeGeneratorAttributeFactory : Attribute, IFilterFactory
+    {
+        public bool IsReusable => true;
+
+        public IFilterMetadata CreateInstance(IServiceProvider serviceProvider)
+        {
+            if (serviceProvider == null) throw new ArgumentNullException(nameof(serviceProvider));
+
+            return serviceProvider.GetRequiredService<DecoyTimeGeneratorAttribute>();
+        } 
+    }
+}
