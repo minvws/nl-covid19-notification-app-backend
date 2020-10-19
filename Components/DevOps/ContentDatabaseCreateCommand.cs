059bcb6ad9c79f8f0a2caf5100263b33152c8096
Sabine Kos <sabinekos@gmail.com>
2020-10-19 15:51:25 +0200

    Removed unused code

--- Components/DevOps/ContentDatabaseCreateCommand.cs
+++ Components/DevOps/ContentDatabaseCreateCommand.cs
@@ -2,13 +2,11 @@
 // Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
 // SPDX-License-Identifier: EUPL-1.2
 
-using System;
-using System.IO;
-using System.Threading.Tasks;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Content;
-using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.EfDatabase;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.EfDatabase.Contexts;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services;
+using System;
+using System.Threading.Tasks;
 
 namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.DevOps
 {
@@ -32,40 +30,6 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.DevOps
             if (nuke)
                 await _DbContextProvider.Database.EnsureDeletedAsync();
             await _DbContextProvider.Database.EnsureCreatedAsync();
-        }
-
-        public async Task AddInitialContent()
-        {
-            await using var tx = await _DbContextProvider.Database.BeginTransactionAsync();
-            await Write(ContentTypes.RiskCalculationParameters, "RiskCalcDefaults.json");
-            await Write(ContentTypes.AppConfig, "AppConfigDefaults.json");
-            await Write(ContentTypes.ResourceBundle, "ResourceBundleDefaults.json");
-            _DbContextProvider.SaveAndCommit();
-        }
-
-        private string ReadFromResource(string resourceName)
-        {
-            var a = System.Reflection.Assembly.GetExecutingAssembly();
-            var manifestResourceStream = a.GetManifestResourceStream($"NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Resources.{resourceName}");
-
-            if (manifestResourceStream == null)
-                throw new InvalidOperationException("Resource not found.");
-
-            using var s = new StreamReader(manifestResourceStream);
-            return s.ReadToEnd();
-        }
-
-        private async Task Write(string type, string resource)
-        {
-            var rcd = ReadFromResource(resource);
-            var args = new ContentArgs
-            {
-                Release = _DateTimeProvider.Snapshot,
-                ContentType = type,
-                Json = rcd
-            };
-            if (!_Validator.IsValid(args)) throw new InvalidOperationException();
-            await _InsertDbCommand.Execute(args);
         }
     }
 }
