7645411334748f146b7d683cf2b338af1c3fc639
Yorim <yorimchristiaanse@protonmail.com>
2020-10-19 10:35:31 +0200

    Creation of Codex and added logcodes for logs in the WorkflowController

---
+++ Components/Logging/LoggingCodex.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Logging
+{
+    internal static class LoggingCodex
+    {
+        //Current ordering is by project; keeping the initial numbering in place.
+        //Under MobileAppApi
+        public const int Register = 100;
+        public const int PostTeks = 200;
+        public const int Decoy = 300;
+    }
+
+    public static class LoggingDataRegisterSecret
+    {
+        public const string Name = "Register";
+
+        public const int Start = LoggingCodex.Register;
+        public const int Finished = LoggingCodex.Register + 99;
+
+        public const int Writing = LoggingCodex.Register + 1;
+        public const int Committed = LoggingCodex.Register + 2;
+        public const int DuplicatesFound = LoggingCodex.Register + 3;
+
+        public const int MaximumCreateAttemptsReached = LoggingCodex.Register + 4;
+        public const int Failed = LoggingCodex.Register + 5;
+    }
+
+    public static class LoggingDataPostKeys
+    {
+        public const string Name = "Postkeys";
+        public const int Start = LoggingCodex.PostTeks;
+        public const int Finished = LoggingCodex.PostTeks + 99;
+
+        public const int SignatureValidationFailed = LoggingCodex.PostTeks + 1;
+        public const int BucketIdParsingFailed = LoggingCodex.PostTeks + 2;
+        public const int TekValidationFailed = LoggingCodex.PostTeks + 3;
+        public const int ValidTekCount = LoggingCodex.PostTeks + 4;
+        public const int BucketDoesNotExist = LoggingCodex.PostTeks + 5;
+        public const int SignatureInvalid = LoggingCodex.PostTeks + 6;
+        public const int WorkflowFilterResults = LoggingCodex.PostTeks + 7;
+        public const int ValidTekCountSecondPass = LoggingCodex.PostTeks + 8;
+        public const int TekDuplicatesFoundWholeWorkflow = LoggingCodex.PostTeks + 9;
+        public const int DbWriteStart = LoggingCodex.PostTeks + 10;
+        public const int DbWriteCommitted = LoggingCodex.PostTeks + 11;
+        public const int TekCountAdded = LoggingCodex.PostTeks + 12;
+    }
+
+    public static class LoggingDataDecoy
+    {
+        public const string Name = "Decoykeys(PostSecret)";
+
+        public const int Start = LoggingCodex.Decoy;
+    }
+
+
+
+}
