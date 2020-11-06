4704d4193e2e941844eda4c6b4e4d59e24144922
Yorim <y.christiaanse@minvwsnl.onmicrosoft.com>
2020-11-06 12:17:47 +0100

    Set logcodes to use four-digit values.

--- Components/Logging/LoggingCodex.cs
+++ Components/Logging/LoggingCodex.cs
@@ -6,33 +6,32 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Logging
 {
     public static class LoggingCodex
     {
-        //Current ordering is by project; keeping the initial numbering in place.
-        //Under MobileAppApi
-        public const int Register = 100;
-        public const int PostTeks = 200;
-        public const int Decoy = 300;
-        public const int ResponsePadding = 400;
-        public const int ExceptionInterceptor = 500;
-        public const int SuppressError = 600;
-        public const int GetCdnContent = 700;
-        public const int IccBackend = 800;
-        public const int DbProvision = 900;
-        public const int PublishContent = 1000;
-        public const int SigtestFileCreator = 1100;
-        public const int DailyCleanup = 1200;
-        public const int RemoveExpiredManifest = 1300;
-        public const int RemoveExpiredEks = 1400;
-        public const int RemoveExpiredWorkflow = 1500;
-        public const int Resigner = 1600;
-        public const int RemoveExpiredEksV2 = 1700;
-        public const int RemoveExpiredManifestV2 = 1800;
-        public const int EksEngine = 1900;
-        public const int Snapshot = 2000;
-        public const int EksBuilderV1 = 2100;
-        public const int EksJobContentWriter = 2200;
-        public const int MarkWorkflowTeksAsUsed = 2300;
-        public const int ManifestUpdate = 2400;
-        public const int EmbeddedCertProvider = 2500;
-        public const int CertLmProvider = 2600;
+        //Current ordering is mainly by project
+        public const int Register = 1100;
+        public const int PostTeks = 1200;
+        public const int Decoy = 1300;
+        public const int ResponsePadding = 1400;
+        public const int ExceptionInterceptor = 1500;
+        public const int SuppressError = 1600;
+        public const int GetCdnContent = 1700;
+        public const int IccBackend = 1800;
+        public const int DbProvision = 1900;
+        public const int PublishContent = 2000;
+        public const int SigtestFileCreator = 2100;
+        public const int DailyCleanup = 2200;
+        public const int RemoveExpiredManifest = 2300;
+        public const int RemoveExpiredEks = 2400;
+        public const int RemoveExpiredWorkflow = 2500;
+        public const int Resigner = 2600;
+        public const int RemoveExpiredEksV2 = 2700;
+        public const int RemoveExpiredManifestV2 = 2800;
+        public const int EksEngine = 2900;
+        public const int Snapshot = 3000;
+        public const int EksBuilderV1 = 3100;
+        public const int EksJobContentWriter = 3200;
+        public const int MarkWorkflowTeksAsUsed = 3300;
+        public const int ManifestUpdate = 3400;
+        public const int EmbeddedCertProvider = 3500;
+        public const int CertLmProvider = 3600;
     }
 }
