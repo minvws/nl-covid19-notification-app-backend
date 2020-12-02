2d258b9ff43e449d1fc4195a303a55ec41e4cd8b
Ryan Barrett <ryan@seldonplan.com>
2020-12-02 13:10:51 +0100

    Updated the batches to use the new file paths.

--- src/copy-CIBG-configs.bat
+++ src/copy-CIBG-configs.bat
@@ -1,18 +1,18 @@
-echo f|xcopy ..\s\publish\BatchJobsApi\appsettings.release.json Artifacts\BatchJobsApi\appsettings.json /Y
-echo f|xcopy ..\s\publish\ContentApi\appsettings.release.json Artifacts\ContentApi\appsettings.json /Y
-echo f|xcopy ..\s\publish\IccBackend\appsettings.release.json Artifacts\IccBackend\appsettings.json /Y
-echo f|xcopy ..\s\publish\MobileAppApi\appsettings.release.json Artifacts\MobileAppApi\appsettings.json /Y
-echo f|xcopy ..\s\publish\ManagementPortal\appsettings.release.json Artifacts\ManagementPortal\appsettings.json /Y
+echo f|xcopy ..\s\src\publish\BatchJobsApi\appsettings.release.json Artifacts\BatchJobsApi\appsettings.json /Y
+echo f|xcopy ..\s\src\publish\ContentApi\appsettings.release.json Artifacts\ContentApi\appsettings.json /Y
+echo f|xcopy ..\s\src\publish\IccBackend\appsettings.release.json Artifacts\IccBackend\appsettings.json /Y
+echo f|xcopy ..\s\src\publish\MobileAppApi\appsettings.release.json Artifacts\MobileAppApi\appsettings.json /Y
+echo f|xcopy ..\s\src\publish\ManagementPortal\appsettings.release.json Artifacts\ManagementPortal\appsettings.json /Y
 
 REM double check later
-echo f|xcopy ..\s\EksEngine\appsettings.release.json Artifacts\EksEngine\appsettings.json /Y
-echo f|xcopy ..\s\ManifestEngine\appsettings.release.json Artifacts\ManifestEngine\appsettings.json /Y
-echo f|xcopy ..\s\DailyCleanup\appsettings.release.json Artifacts\DailyCleanup\appsettings.json /Y
-echo f|xcopy ..\s\EfgsDownloader\appsettings.release.json Artifacts\EfgsDownloader\appsettings.json /Y
-echo f|xcopy ..\s\EfgsUploader\appsettings.release.json Artifacts\EfgsUploader\appsettings.json /Y
+echo f|xcopy ..\s\src\EksEngine\appsettings.release.json Artifacts\EksEngine\appsettings.json /Y
+echo f|xcopy ..\s\src\ManifestEngine\appsettings.release.json Artifacts\ManifestEngine\appsettings.json /Y
+echo f|xcopy ..\s\src\DailyCleanup\appsettings.release.json Artifacts\DailyCleanup\appsettings.json /Y
+echo f|xcopy ..\s\src\EfgsDownloader\appsettings.release.json Artifacts\EfgsDownloader\appsettings.json /Y
+echo f|xcopy ..\s\src\EfgsUploader\appsettings.release.json Artifacts\EfgsUploader\appsettings.json /Y
 
-echo f|xcopy ..\s\DbProvision\appsettings.release.json Artifacts\DbProvision\appsettings.json /Y
-echo f|xcopy ..\s\PublishContent\appsettings.release.json Artifacts\PublishContent\appsettings.json /Y
-echo f|xcopy ..\s\GenTeks\appsettings.release.json Artifacts\GenTeks\appsettings.json /Y
-echo f|xcopy ..\s\ForceTekAuth\appsettings.release.json Artifacts\ForceTekAuth\appsettings.json /Y
-echo f|xcopy ..\s\SigTestFileCreator\appsettings.release.json Artifacts\SigTestFileCreator\appsettings.json /Y
+echo f|xcopy ..\s\src\DbProvision\appsettings.release.json Artifacts\DbProvision\appsettings.json /Y
+echo f|xcopy ..\s\src\PublishContent\appsettings.release.json Artifacts\PublishContent\appsettings.json /Y
+echo f|xcopy ..\s\src\GenTeks\appsettings.release.json Artifacts\GenTeks\appsettings.json /Y
+echo f|xcopy ..\s\src\ForceTekAuth\appsettings.release.json Artifacts\ForceTekAuth\appsettings.json /Y
+echo f|xcopy ..\s\src\SigTestFileCreator\appsettings.release.json Artifacts\SigTestFileCreator\appsettings.json /Y
--- src/package-CIBG.bat
+++ src/package-CIBG.bat
@@ -16,33 +16,33 @@ REM 7z a -tzip packages\App\Website.ContentApi.zip publish\ContentApi"
 REM 7z a -tzip packages\App\Website.IccBackend.zip publish\IccBackend"
 REM 7z a -tzip packages\App\Website.MobileAppApi.zip publish\MobileAppApi"
 
-xcopy ..\s\publish\BatchJobsApi Artifacts\BatchJobsApi /E /I
-xcopy ..\s\publish\ContentApi Artifacts\ContentApi /E /I
-xcopy ..\s\publish\IccBackend Artifacts\IccBackend /E /I
-xcopy ..\s\publish\MobileAppApi Artifacts\MobileAppApi /E /I
-xcopy ..\s\publish\ManagementPortal Artifacts\ManagementPortal /E /I
+xcopy ..\s\src\publish\BatchJobsApi Artifacts\BatchJobsApi /E /I
+xcopy ..\s\src\publish\ContentApi Artifacts\ContentApi /E /I
+xcopy ..\s\src\publish\IccBackend Artifacts\IccBackend /E /I
+xcopy ..\s\src\publish\MobileAppApi Artifacts\MobileAppApi /E /I
+xcopy ..\s\src\publish\ManagementPortal Artifacts\ManagementPortal /E /I
 
 REM Zip command-line
 REM 7z a -tzip packages\Batch\CommandLine.EksEngine.zip publish\Batch.EksEngine"
 REM 7z a -tzip packages\Batch\CommandLine.ManifestEngine.zip publish\Batch.ManifestEngine"
 
-xcopy ..\s\publish\EksEngine Artifacts\EksEngine /E /I
-xcopy ..\s\publish\ManifestEngine Artifacts\ManifestEngine /E /I
-xcopy ..\s\publish\DailyCleanup Artifacts\DailyCleanup /E /I
-xcopy ..\s\publish\EfgsDownloader Artifacts\EfgsDownloader /E /I
-xcopy ..\s\publish\EfgsUploader Artifacts\EfgsUploader /E /I
+xcopy ..\s\src\publish\EksEngine Artifacts\EksEngine /E /I
+xcopy ..\s\src\publish\ManifestEngine Artifacts\ManifestEngine /E /I
+xcopy ..\s\src\publish\DailyCleanup Artifacts\DailyCleanup /E /I
+xcopy ..\s\src\publish\EfgsDownloader Artifacts\EfgsDownloader /E /I
+xcopy ..\s\src\publish\EfgsUploader Artifacts\EfgsUploader /E /I
 
 REM Database
 REM 7z a -tzip packages\SQL\SQL.DbProvision.zip publish\Tools\DbProvision"
 
-xcopy ..\s\publish\Tools\DbProvision Artifacts\DbProvision /E /I
-xcopy ..\s\publish\Tools\PublishContent Artifacts\PublishContent /E /I
-xcopy ..\s\publish\Tools\GenTeks Artifacts\GenTeks /E /I
-xcopy ..\s\publish\Tools\ForceTekAuth Artifacts\ForceTekAuth /E /I
-xcopy ..\s\publish\Tools\SigTestFileCreator Artifacts\SigTestFileCreator /E /I
-xcopy ..\s\publish\Tools\EksParser Artifacts\EksParser /E /I
+xcopy ..\s\src\publish\Tools\DbProvision Artifacts\DbProvision /E /I
+xcopy ..\s\src\publish\Tools\PublishContent Artifacts\PublishContent /E /I
+xcopy ..\s\src\publish\Tools\GenTeks Artifacts\GenTeks /E /I
+xcopy ..\s\src\publish\Tools\ForceTekAuth Artifacts\ForceTekAuth /E /I
+xcopy ..\s\src\publish\Tools\SigTestFileCreator Artifacts\SigTestFileCreator /E /I
+xcopy ..\s\src\publish\Tools\EksParser Artifacts\EksParser /E /I
 
-xcopy ..\s\HSM-Scripting Artifacts\HSM-Scripting /E /I
+xcopy ..\s\src\HSM-Scripting Artifacts\HSM-Scripting /E /I
 
 REM Zip tools
 REM 7z a -tzip packages\Tools\CommandLine.GenTeks.zip publish\Tools\GenTeks"
