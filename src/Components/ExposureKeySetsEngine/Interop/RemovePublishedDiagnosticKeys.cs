c14b4a6eb1bd30483b81dec134d1f372b677e14d
Steve Kellaway <steve.kellaway@mefitihe.com>
2020-12-15 15:32:30 +0100

    Added 14 day cutoff

--- src/Components/ExposureKeySetsEngine/Interop/RemovePublishedDiagnosticKeys.cs
+++ src/Components/ExposureKeySetsEngine/Interop/RemovePublishedDiagnosticKeys.cs
@@ -7,6 +7,7 @@ using Microsoft.EntityFrameworkCore;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.EfDatabase;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.EfDatabase.Contexts;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.EfDatabase.Entities;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Services;
 
 namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySetsEngine.Interop
 {
@@ -14,10 +15,12 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySe
     {
         private RemovePublishedDiagnosticKeysResult _Result;
         private readonly Func<DkSourceDbContext> _DiagnosticKeyDbContextProvider;
+        private readonly IUtcDateTimeProvider _UtcDateTimeProvider;
 
-        public RemovePublishedDiagnosticKeys(Func<DkSourceDbContext> diagnosticKeyDbContextProvider)
+        public RemovePublishedDiagnosticKeys(Func<DkSourceDbContext> diagnosticKeyDbContextProvider, IUtcDateTimeProvider utcDateTimeProvider)
         {
             _DiagnosticKeyDbContextProvider = diagnosticKeyDbContextProvider ?? throw new ArgumentNullException(nameof(diagnosticKeyDbContextProvider));
+            _UtcDateTimeProvider = utcDateTimeProvider ?? throw new ArgumentNullException(nameof(utcDateTimeProvider));
         }
 
         public RemovePublishedDiagnosticKeysResult Execute()
@@ -27,11 +30,14 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.ExposureKeySe
 
             _Result = new RemovePublishedDiagnosticKeysResult();
 
+            //TODO setting
+            var cutoff = _UtcDateTimeProvider.Snapshot.AddDays(-14).Date.ToRollingStartNumber();
+
             using (var dbc = _DiagnosticKeyDbContextProvider())
             {
                 using (var tx = dbc.BeginTransaction())
                 {
-                    _Result.GivenMercy = dbc.Database.ExecuteSqlRaw($"DELETE FROM {TableNames.DiagnosisKeys} WHERE [PublishedLocally] = 1 AND [PublishedToEfgs] = 1;");
+                    _Result.GivenMercy = dbc.Database.ExecuteSqlRaw($"DELETE FROM {TableNames.DiagnosisKeys} WHERE [PublishedLocally] = 1 AND [PublishedToEfgs] = 1 AND DailyKey_RollingStartNumber < {cutoff};");
                     tx.Commit();
                 }
             }
