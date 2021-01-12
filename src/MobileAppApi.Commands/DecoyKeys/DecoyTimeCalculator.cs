c78d43c1f89adbce2ecfec8b5872bfd2de75db1c
Yorim <y.christiaanse@minvwsnl.onmicrosoft.com>
2021-01-12 15:52:54 +0100

    DecoyTimeCalculator logic for new layout

---
+++ src/MobileAppApi.Commands/DecoyKeys/DecoyTimeCalculator.cs
@@ -0,0 +1,62 @@
+﻿﻿// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+using System;
+using System.Threading.Tasks;
+using MathNet.Numerics.Distributions;
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.MobileAppApi.Commands.DecoyKeys
+{
+	public class DecoyTimeCalculator
+	{
+		private readonly DecoyKeysLoggingExtensions _Logger;
+
+		private int _Count { get; set; }
+		private double _SumSquareDiff { get; set; }
+
+		public double DecoyTimeMean { get; private set; }
+		public double DecoyTimeStDev => _Count > 1 ? Math.Sqrt(_SumSquareDiff / (_Count - 1)) : 0;
+
+		public DecoyTimeCalculator(DecoyKeysLoggingExtensions logger)
+		{
+			_Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+			_Count = 0;
+			DecoyTimeMean = 0;
+			_SumSquareDiff = 0;
+		}
+
+		public void RegisterTime(double timeMs) //Welford's algorithm
+		{
+			if (timeMs <= 0)
+			{
+				throw new ArgumentOutOfRangeException($"RegisterTime was called with a non-positive time: {timeMs}");
+			}
+
+			_Count++;
+
+			if (_Count == 1)
+			{
+				DecoyTimeMean = timeMs;
+			}
+			else
+			{
+				var newMean = DecoyTimeMean + (timeMs - DecoyTimeMean) / _Count;
+				_SumSquareDiff = _SumSquareDiff + (timeMs - DecoyTimeMean) * (timeMs - newMean);
+				DecoyTimeMean = newMean;
+			}
+
+			_Logger.WriteTimeRegistered(_Count, timeMs, DecoyTimeMean, DecoyTimeStDev);
+		}
+
+		public async Task PerformDecoyWaitAsync()
+		{
+			var gaussian = new Normal(DecoyTimeMean, DecoyTimeStDev);
+			var timeMs = (int)gaussian.Sample();
+
+			_Logger.WriteGeneratingDelay(timeMs);
+			await Task.Delay(timeMs);
+		}
+	}
+}
---
+++ src/MobileAppApi.Tests/DecoyKeys/DecoyTimeCalculatorTest.cs
@@ -0,0 +1,96 @@
+﻿// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+using System;
+using System.Collections.Generic;
+using System.Collections;
+using Xunit;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.MobileAppApi.Commands.DecoyKeys;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Components.Tests.DecoyTime
+{
+	public class DecoyTimeCalculatorTest
+	{
+		private DecoyKeysLoggingExtensions _Logger;
+
+		public DecoyTimeCalculatorTest()
+		{
+			var loggerfactory = new LoggerFactory();
+
+			_Logger = new DecoyKeysLoggingExtensions(loggerfactory.CreateLogger<DecoyKeysLoggingExtensions>());
+		}
+
+		[Theory]
+		[ClassData(typeof(DecoyTimeTestData))]
+		public void RegisterTime_CalculateMeanAndStdDev(double[] inputValues, double expectedMean, double expectedStDev)
+		{
+			//Arrange
+			var sut = new DecoyTimeCalculator(_Logger);
+
+			//Act
+			foreach (var dataPoint in inputValues)
+			{
+				sut.RegisterTime(dataPoint);
+			}
+
+			var resultMean = sut.DecoyTimeMean;
+			var resultStDev = sut.DecoyTimeStDev;
+
+			//Assert
+			Assert.Equal(expectedMean, resultMean, 2);
+			Assert.Equal(expectedStDev, resultStDev, 2);
+		}
+
+		[Theory]
+		[InlineData(0.0)]
+		[InlineData(-12.4)]
+		public void RegisterTime_ThrowsOnNonPositiveInput(double incorrectTime)
+		{
+			//Arrange
+			var sut = new DecoyTimeCalculator(_Logger);
+			Action runSut = () => sut.RegisterTime(incorrectTime);
+
+			//Assert
+			Assert.Throws<ArgumentOutOfRangeException>(runSut);
+		}
+
+		[Theory]
+		[InlineData(42)]
+		[InlineData(343)]
+		[InlineData(2416)]
+		[InlineData(10000)]
+		public async void PerformDecoyWaitAsync_ExecutionTakesAtleastNMilliseconds(int timeMs)
+		{
+			//Arrange
+			var sut = new DecoyTimeCalculator(_Logger);
+			var timer = new Stopwatch();
+
+			//Act
+			sut.RegisterTime(timeMs);
+			timer.Start();
+			await sut.PerformDecoyWaitAsync();
+			timer.Stop();
+
+			//Assert
+			Assert.True(timer.ElapsedMilliseconds >= timeMs, $"Recorded time: {timer.ElapsedMilliseconds}ms.");
+		}
+
+		private class DecoyTimeTestData : IEnumerable<object[]>
+		{
+			public IEnumerator<object[]> GetEnumerator()
+			{
+				yield return new object[] { new double[] { }, 0, 0 };
+				yield return new object[] { new double[] { 10.0 }, 10.0, 0 };
+				yield return new object[] { new double[] { 10.0, 10.0 }, 10.0, 0 };
+				yield return new object[] { new double[] { 10.0, 9.0, 11.0 }, 10.0, 1.0 };
+				yield return new object[] { new double[] { 2, 4, 6, 8, 10, 12, 14, 17, 20 }, 10.33, 6 };
+				yield return new object[] { new double[] { 3.5, 7.2, 1.1, 0.2 }, 3, 3.1273 };
+			}
+
+			IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+		}
+	}
+}
