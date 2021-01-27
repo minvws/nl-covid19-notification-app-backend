075188f7cb095595b35d18feb8d1560900d9de95
Sabine Kos <sabinekos@gmail.com>
2021-01-27 13:39:43 +0100

    Corrected left-hand path of xcopy command

--- src/copy-CIBG-configs.bat
+++ src/copy-CIBG-configs.bat
@@ -8,8 +8,8 @@ REM double check later
 echo f|xcopy ..\s\src\EksEngine\appsettings.release.json Artifacts\EksEngine\appsettings.json /Y
 echo f|xcopy ..\s\src\ManifestEngine\appsettings.release.json Artifacts\ManifestEngine\appsettings.json /Y
 echo f|xcopy ..\s\src\DailyCleanup\appsettings.release.json Artifacts\DailyCleanup\appsettings.json /Y
-echo f|xcopy ..\s\src\EfgsDownloader\appsettings.release.json Artifacts\EfgsDownloader\appsettings.json /Y
-echo f|xcopy ..\s\src\EfgsUploader\appsettings.release.json Artifacts\EfgsUploader\appsettings.json /Y
+echo f|xcopy ..\s\src\Iks.Downloader\appsettings.release.json Artifacts\EfgsDownloader\appsettings.json /Y
+echo f|xcopy ..\s\src\Iks.Uploader\appsettings.release.json Artifacts\EfgsUploader\appsettings.json /Y
 
 echo f|xcopy ..\s\src\PublishContent\appsettings.release.json Artifacts\PublishContent\appsettings.json /Y
 echo f|xcopy ..\s\src\SigTestFileCreator\appsettings.release.json Artifacts\SigTestFileCreator\appsettings.json /Y
