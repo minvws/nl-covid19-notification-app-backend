00d169e9fbf61cebf5b06c244814638116e22423
Michel van der Ende <m.vd.ende@minvws.nl>
2021-02-26 16:22:14 +0100

    Remove ALTER add DEFAULT DaysSinceSymptomsOnset

--- src/Database/PublishingJob/dbo/Tables/EksCreateJobInput.sql
+++ src/Database/PublishingJob/dbo/Tables/EksCreateJobInput.sql
@@ -1,29 +1,18 @@
-﻿
-CREATE TABLE [dbo].[EksCreateJobInput](
+﻿CREATE TABLE [dbo].[EksCreateJobInput](
 	[Id] [bigint] IDENTITY(1,1) NOT NULL,
 	[TekId] [bigint] NULL,
 	[Used] [bit] NOT NULL,
 	[KeyData] [varbinary](900) NOT NULL,
 	[RollingStartNumber] [int] NOT NULL,
 	[RollingPeriod] [int] NOT NULL,
-	[TransmissionRiskLevel] [int] NOT NULL,
-	[DaysSinceSymptomsOnset] [int] NULL,
+	[DaysSinceSymptomsOnset] [int] NOT NULL DEFAULT 0,
+	[IsInfectious] [bit] NOT NULL DEFAULT 0,
  CONSTRAINT [PK_EksCreateJobInput] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
 ) ON [PRIMARY]
 GO
-
-ALTER TABLE [dbo].[EksCreateJobInput] ADD  CONSTRAINT [DF_EksCreateJobInput_TransmissionRiskLevel]  DEFAULT ((0)) FOR [TransmissionRiskLevel]
-GO
-
-ALTER TABLE [dbo].[EksCreateJobInput] ADD  CONSTRAINT [DF_EksCreateJobInput_DaysSinceSymptomsOnset]  DEFAULT ((0)) FOR [DaysSinceSymptomsOnset]
-GO
-
-CREATE NONCLUSTERED INDEX [IX_EksCreateJobInput_TransmissionRiskLevel]
-    ON [dbo].[EksCreateJobInput]([TransmissionRiskLevel] ASC);
-
 
 GO
 CREATE UNIQUE NONCLUSTERED INDEX [IX_EksCreateJobInput_TekId]
@@ -33,4 +22,3 @@ CREATE UNIQUE NONCLUSTERED INDEX [IX_EksCreateJobInput_TekId]
 GO
 CREATE NONCLUSTERED INDEX [IX_EksCreateJobInput_KeyData]
     ON [dbo].[EksCreateJobInput]([KeyData] ASC);
-
