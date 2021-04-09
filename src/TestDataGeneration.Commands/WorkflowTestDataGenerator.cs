6560f50b2e3473e935e9e7cad1e3144c7affac3b
Sabine Kos <sabinekos@gmail.com>
2021-04-09 12:38:07 +0200

    Fixed up tests; casing; removed unused/commented code

--- src/TestDataGeneration.Commands/WorkflowTestDataGenerator.cs
+++ src/TestDataGeneration.Commands/WorkflowTestDataGenerator.cs
@@ -12,6 +12,7 @@ using NL.Rijksoverheid.ExposureNotification.BackEnd.Core.EntityFramework;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.DiagnosisKeys.EntityFramework;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.DiagnosisKeys.Processors;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.Domain;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Domain.LuhnModN;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.EksEngine.Commands.DiagnosisKeys.Commands;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.MobileAppApi.Commands.RegisterSecret;
 using NL.Rijksoverheid.ExposureNotification.BackEnd.MobileAppApi.Workflow.EntityFramework;
@@ -22,19 +23,21 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.TestDataGeneration.Comma
 {
     public class WorkflowTestDataGenerator
     {
-        private readonly IDbProvider<WorkflowDbContext> _WorkflowDbContextProvider;
-        private readonly IDbProvider<DkSourceDbContext> _DkSourceDbContextProvider;
-        private readonly ILoggerFactory _LoggerFactory = new SerilogLoggerFactory();
-        private readonly IUtcDateTimeProvider _UtcDateTimeProvider = new StandardUtcDateTimeProvider();
-        private readonly StandardRandomNumberGenerator _Rng = new StandardRandomNumberGenerator();
-        private readonly IWrappedEfExtensions _EfExtensions;
-        private readonly Mock<IOutboundFixedCountriesOfInterestSetting> _CountriesOut = new Mock<IOutboundFixedCountriesOfInterestSetting>();
+        private readonly IDbProvider<WorkflowDbContext> _workflowDbContextProvider;
+        private readonly IDbProvider<DkSourceDbContext> _dkSourceDbContextProvider;
+        private readonly ILoggerFactory _loggerFactory = new SerilogLoggerFactory();
+        private readonly IUtcDateTimeProvider _utcDateTimeProvider = new StandardUtcDateTimeProvider();
+        private readonly StandardRandomNumberGenerator _rng = new StandardRandomNumberGenerator();
+        private readonly IWrappedEfExtensions _efExtensions;
+        private readonly Mock<IOutboundFixedCountriesOfInterestSetting> _countriesOut = new Mock<IOutboundFixedCountriesOfInterestSetting>();
+        private readonly ILuhnModNConfig _luhnModNConfig = new LuhnModNConfig();
+        private Mock<ILuhnModNGenerator> _luhnModNGeneratorMock;
 
         public WorkflowTestDataGenerator(IDbProvider<WorkflowDbContext> workflowDbContextProvider, IDbProvider<DkSourceDbContext> dkSourceDbContextProvider, IWrappedEfExtensions efExtensions)
         {
-            _WorkflowDbContextProvider = workflowDbContextProvider ?? throw new ArgumentNullException(nameof(workflowDbContextProvider));
-            _DkSourceDbContextProvider = dkSourceDbContextProvider ?? throw new ArgumentNullException(nameof(dkSourceDbContextProvider));
-            _EfExtensions = efExtensions ?? throw new ArgumentNullException(nameof(efExtensions));
+            _workflowDbContextProvider = workflowDbContextProvider ?? throw new ArgumentNullException(nameof(workflowDbContextProvider));
+            _dkSourceDbContextProvider = dkSourceDbContextProvider ?? throw new ArgumentNullException(nameof(dkSourceDbContextProvider));
+            _efExtensions = efExtensions ?? throw new ArgumentNullException(nameof(efExtensions));
         }
 
         public async Task<int> GenerateAndAuthoriseWorkflowsAsync(int workflowCount = 25, int tekPerWOrkflowCount = 4)
@@ -42,61 +45,66 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.TestDataGeneration.Comma
             await GenerateWorkflowsAsync(workflowCount, tekPerWOrkflowCount);
             await AuthoriseAllWorkflowsAsync();
             await SnapshotToDks();
-            return _DkSourceDbContextProvider.CreateNew().DiagnosisKeys.Count();
+            return _dkSourceDbContextProvider.CreateNew().DiagnosisKeys.Count();
         }
 
         public async Task SnapshotToDks()
         {
-            _CountriesOut.Setup(x => x.CountriesOfInterest).Returns(new[] { "DE" });
-            await new SnapshotWorkflowTeksToDksCommand(_LoggerFactory.CreateLogger<SnapshotWorkflowTeksToDksCommand>(),
-                _UtcDateTimeProvider,
+            _countriesOut.Setup(x => x.CountriesOfInterest).Returns(new[] { "DE" });
+            await new SnapshotWorkflowTeksToDksCommand(_loggerFactory.CreateLogger<SnapshotWorkflowTeksToDksCommand>(),
+                _utcDateTimeProvider,
                 new TransmissionRiskLevelCalculationMk2(),
-                _WorkflowDbContextProvider.CreateNew(),
-                _WorkflowDbContextProvider.CreateNew,
-                _DkSourceDbContextProvider.CreateNew,
-                _EfExtensions,
+                _workflowDbContextProvider.CreateNew(),
+                _workflowDbContextProvider.CreateNew,
+                _dkSourceDbContextProvider.CreateNew,
+                _efExtensions,
                 new IDiagnosticKeyProcessor[] {
                     new ExcludeTrlNoneDiagnosticKeyProcessor(),
-                    new FixedCountriesOfInterestOutboundDiagnosticKeyProcessor(_CountriesOut.Object),
+                    new FixedCountriesOfInterestOutboundDiagnosticKeyProcessor(_countriesOut.Object),
                     new NlToEfgsDsosDiagnosticKeyProcessorMk1()
                 }
             ).ExecuteAsync();
         }
 
-        public async Task GenerateWorkflowsAsync(int workflowCount = 25, int tekPerWOrkflowCount = 4)
+        public async Task GenerateWorkflowsAsync(int workflowCount = 25, int tekPerWorkflowCount = 4)
         {
             var m1 = new Mock<IWorkflowConfig>(MockBehavior.Strict);
             m1.Setup(x => x.TimeToLiveMinutes).Returns(10000);
             m1.Setup(x => x.PermittedMobileDeviceClockErrorMinutes).Returns(30);
-            
+
+            _luhnModNGeneratorMock = new Mock<ILuhnModNGenerator>();
+
             Func<TekReleaseWorkflowStateCreate> createWf = () =>
-                new TekReleaseWorkflowStateCreate(_WorkflowDbContextProvider.CreateNewWithTx(),
-                    _UtcDateTimeProvider, _Rng,
-                    new LabConfirmationIdService(_Rng),
+                new TekReleaseWorkflowStateCreate(
+                    _workflowDbContextProvider.CreateNewWithTx(),
+                    _utcDateTimeProvider,
+                    _rng,
+                    new LabConfirmationIdService(_rng),
                     new TekReleaseWorkflowTime(m1.Object),
-                    m1.Object,
-                    new RegisterSecretLoggingExtensions(_LoggerFactory.CreateLogger<RegisterSecretLoggingExtensions>())
+                    new RegisterSecretLoggingExtensions(_loggerFactory.CreateLogger<RegisterSecretLoggingExtensions>()),
+                    _luhnModNConfig,
+                    _luhnModNGeneratorMock.Object
                 );
 
-            var gen = new GenerateTeksCommand(_Rng, _WorkflowDbContextProvider.CreateNewWithTx, createWf);
-            await gen.ExecuteAsync(new GenerateTeksCommandArgs { WorkflowCount = workflowCount , TekCountPerWorkflow = tekPerWOrkflowCount });
+            var gen = new GenerateTeksCommand(_rng, _workflowDbContextProvider.CreateNewWithTx, createWf);
+            await gen.ExecuteAsync(new GenerateTeksCommandArgs { WorkflowCount = workflowCount, TekCountPerWorkflow = tekPerWorkflowCount });
 
-            if (workflowCount != _WorkflowDbContextProvider.CreateNew().KeyReleaseWorkflowStates.Count()) throw new InvalidOperationException();
-            if (workflowCount * tekPerWOrkflowCount != _WorkflowDbContextProvider.CreateNew().TemporaryExposureKeys.Count()) throw new InvalidOperationException();
+            if (workflowCount != _workflowDbContextProvider.CreateNew().KeyReleaseWorkflowStates.Count()) throw new InvalidOperationException();
+            if (workflowCount * tekPerWorkflowCount != _workflowDbContextProvider.CreateNew().TemporaryExposureKeys.Count()) throw new InvalidOperationException();
         }
 
         public async Task AuthoriseAllWorkflowsAsync()
         {
-            var wfdb = _WorkflowDbContextProvider.CreateNew();
+            var wfdb = _workflowDbContextProvider.CreateNew();
             foreach (var i in wfdb.KeyReleaseWorkflowStates)
             {
-                i.AuthorisedByCaregiver = _UtcDateTimeProvider.Snapshot;
-                i.DateOfSymptomsOnset = _UtcDateTimeProvider.Snapshot.Date.AddDays(-2);
+                i.AuthorisedByCaregiver = _utcDateTimeProvider.Snapshot;
+                i.DateOfSymptomsOnset = _utcDateTimeProvider.Snapshot.Date.AddDays(-2);
             }
 
             foreach (var i in wfdb.TemporaryExposureKeys)
             {
-                i.PublishAfter = _UtcDateTimeProvider.Snapshot;
+                i.PublishAfter = _utcDateTimeProvider.Snapshot;
             }
 
             await wfdb.SaveChangesAsync();
