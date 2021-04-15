1f32fc8a2bb678f3e53fb0433080976d9b1dfefd
Yorim <y.christiaanse@minvwsnl.onmicrosoft.com>
2021-04-15 17:03:16 +0200

    Idempotence-test with DateTimeMock

--- src/ManifestEngine.Tests/ManifestUpdateCommandTest.cs
+++ src/ManifestEngine.Tests/ManifestUpdateCommandTest.cs
@@ -22,6 +22,8 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.ManifestEngine.Tests
     {
         private readonly IDbProvider<ContentDbContext> _contentDbProvider;
         private readonly ManifestUpdateCommand _sut;
+        private Mock<IUtcDateTimeProvider> _dateTimeProviderMock;
+        private DateTime _mockedTime = DateTime.UtcNow;
 
         public ManifestUpdateCommandTest(IDbProvider<ContentDbContext> contentDbProvider)
         {
@@ -34,9 +36,12 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.ManifestEngine.Tests
             var eksConfigMock = new Mock<IEksConfig>(MockBehavior.Strict);
             eksConfigMock.Setup(x => x.LifetimeDays)
                 .Returns(14);
-
+ 
+            _dateTimeProviderMock = new Mock<IUtcDateTimeProvider>();
+                _dateTimeProviderMock.Setup(x => x.Snapshot)
+                    .Returns(_mockedTime);
+            
             var loggerFactory = new LoggerFactory();
-            var dateTimeProvider = new StandardUtcDateTimeProvider();
             var jsonSerializer = new StandardJsonSerializer();
             var loggingExtensionsMock = new ManifestUpdateCommandLoggingExtensions(
                 loggerFactory.CreateLogger<ManifestUpdateCommandLoggingExtensions>());
@@ -48,12 +53,12 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.ManifestEngine.Tests
                     jsonSerializer);
 
             _sut = new ManifestUpdateCommand(
-                new ManifestV2Builder(_contentDbProvider.CreateNew(), eksConfigMock.Object, dateTimeProvider),
-                new ManifestV3Builder(_contentDbProvider.CreateNew(), eksConfigMock.Object, dateTimeProvider),
-                new ManifestV4Builder(_contentDbProvider.CreateNew(), eksConfigMock.Object, dateTimeProvider),
+                new ManifestV2Builder(_contentDbProvider.CreateNew(), eksConfigMock.Object, _dateTimeProviderMock.Object),
+                new ManifestV3Builder(_contentDbProvider.CreateNew(), eksConfigMock.Object, _dateTimeProviderMock.Object),
+                new ManifestV4Builder(_contentDbProvider.CreateNew(), eksConfigMock.Object, _dateTimeProviderMock.Object),
                 _contentDbProvider.CreateNew,
                 loggingExtensionsMock,
-                dateTimeProvider,
+                _dateTimeProviderMock.Object,
                 jsonSerializer,
                 ContentFormatterInjector
             );
@@ -70,6 +75,25 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.ManifestEngine.Tests
             Assert.Equal(1, _contentDbProvider.CreateNew().Content.Count(x => x.Type == ContentTypes.ManifestV2));
             Assert.Equal(1, _contentDbProvider.CreateNew().Content.Count(x => x.Type == ContentTypes.ManifestV3)); 
             Assert.Equal(1, _contentDbProvider.CreateNew().Content.Count(x => x.Type == ContentTypes.ManifestV4)); 
+        }
+
+        [Fact]
+        public async Task NoManifestInDb_ExecuteAllTwice_ThreeManifestsInDb()
+        {
+            //Act
+            await _sut.ExecuteAllAsync();
+            
+            //Advance the clock
+            _dateTimeProviderMock.Setup(x => x.Snapshot)
+                .Returns(_mockedTime.AddMinutes(2));
+            
+            await _sut.ExecuteAllAsync();
+
+            //Assert
+            Assert.Equal(3, _contentDbProvider.CreateNew().Content.Count());
+            Assert.Equal(1, _contentDbProvider.CreateNew().Content.Count(x => x.Type == ContentTypes.ManifestV2));
+            Assert.Equal(1, _contentDbProvider.CreateNew().Content.Count(x => x.Type == ContentTypes.ManifestV3));
+            Assert.Equal(1, _contentDbProvider.CreateNew().Content.Count(x => x.Type == ContentTypes.ManifestV4));
         }
 
         public void Dispose()
