1994024fde4fe5f7034ad50501f73daaa9f18fc1
Bert van Nes <b.van.nes@gmail.com>
2021-04-30 12:34:28 +0200

    added UseForwardedHeaders

--- src/Icc.WebApp/Startup.cs
+++ src/Icc.WebApp/Startup.cs
@@ -117,6 +117,8 @@ namespace NL.Rijksoverheid.ExposureNotification.BackEnd.Applications.Icc.WebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseForwardedHeaders();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
