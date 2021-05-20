60d6513d9aef4a2ea2bd28bfe61272a286c48ce2
Bert van Nes <b.van.nes@gmail.com>
2021-05-20 14:31:22 +0200

    added processer to set EFGS (a)symptomatic value to NL value

---
+++ src/DiagnosisKeys.Processors/NlSymptomaticFromDecodedDosDiagnosticKeyProcessor.cs
@@ -0,0 +1,31 @@
+﻿using NL.Rijksoverheid.ExposureNotification.BackEnd.Domain.DsosEncoding;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Domain.Rcp;
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.DiagnosisKeys.Processors
+{
+    public class NlSymptomaticFromDecodedDosDiagnosticKeyProcessor : IDiagnosticKeyProcessor
+    {
+        public DkProcessingItem? Execute(DkProcessingItem? value)
+        {
+            if (value == null) return value;
+
+            if (!value.Metadata.TryGetValue(DosDecodingDiagnosticKeyProcessor.DecodedDsosMetadataKey, out var decodedDos))
+            {
+                //TODO log not present... and fill up the log files...
+                return null;
+            }
+
+            // decodedDos map to InfectiousPeriodType
+            value.DiagnosisKey.Local.Symptomatic = GetSymptomObservation((DsosDecodeResult)decodedDos);
+
+            return value;
+        }
+        
+        private InfectiousPeriodType GetSymptomObservation(DsosDecodeResult value)
+        {
+            return value.SymptomObservation == SymptomObservation.Asymptomatic
+                ? InfectiousPeriodType.Asymptomatic
+                : InfectiousPeriodType.Symptomatic; // Defaults to Symptomatic
+        }
+    }
+}
