36a1d826a804500c27574d970f504021c3a94619
Bert van Nes <b.van.nes@gmail.com>
2021-07-09 10:00:42 +0200

    fixed lint error

--- src/Icc.WebApp/ClientApp/src/app/helpers/error.interceptor.ts
+++ src/Icc.WebApp/ClientApp/src/app/helpers/error.interceptor.ts
@@ -13,8 +13,6 @@ export class ErrorInterceptor implements HttpInterceptor {
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
             if (err.status === 401) {
-                // auto logout if 401 response returned from api
-                //this.authenticationService.logout();
                 // tslint:disable-next-line: deprecation
                 // location.reload(true);
             }
