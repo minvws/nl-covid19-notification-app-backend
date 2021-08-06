bbcd6ba1077f66b996a1c6695b9f65b3fedc7a6c
Bert van Nes <b.van.nes@gmail.com>
2021-08-06 09:53:12 +0200

    Refactored service registrations

---
+++ src/Iks.Downloader/ServiceRegistrations/LoggingExtensionsSetup.cs
@@ -0,0 +1,19 @@
+// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+using Microsoft.Extensions.DependencyInjection;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Crypto.Certificates;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.Iks.Commands.Inbound;
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.EfgsDownloader.ServiceRegistrations
+{
+    public static class LoggingExtensionsSetup
+    {
+        public static void LoggingExtensionsRegistration(this IServiceCollection services)
+        {
+            services.AddSingleton<LocalMachineStoreCertificateProviderLoggingExtensions>();
+            services.AddSingleton<IksDownloaderLoggingExtensions>();
+        }
+    }
+}
