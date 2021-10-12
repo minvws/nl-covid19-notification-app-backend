0a7d891979ab71d8f08bb7237e4536017daa37bf
Bert van Nes <b.van.nes@gmail.com>
2021-10-12 15:42:51 +0200

    database project added

---
+++ src/Database/Icc_DistMemCache/PostDeploy/PostDeploy.sql
@@ -0,0 +1,9 @@
+﻿PRINT 'START POST-DEPLOYMENT ON $(Domain)'
+
+DECLARE @Domain NVARCHAR(3) = '$(Domain)';
+
+:r .\Scripts\Users.sql
+:r .\Scripts\RoleMemberships.sql
+:r .\Scripts\Permissions.sql
+
+PRINT 'FINISHED POST-DEPLOYMENT ON $(Domain)'
