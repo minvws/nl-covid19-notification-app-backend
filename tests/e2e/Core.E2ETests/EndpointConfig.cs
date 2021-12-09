1a753b2bcef54b86083dadd13bfdf8c59648cdae
Bert van Nes <b.van.nes@gmail.com>
2021-12-09 13:09:56 +0100

    added public and internal urls config

--- tests/e2e/Core.E2ETests/EndpointConfig.cs
+++ tests/e2e/Core.E2ETests/EndpointConfig.cs
@@ -14,29 +14,33 @@ namespace Core.E2ETests
         private readonly Dictionary<string, string> _appBaseUrls;
         private readonly Dictionary<string, string> _iccApiBaseUrls;
         private readonly Dictionary<string, string> _iccPortalBaseUrls;
+        private readonly Dictionary<string, string> _cdnBaseUrlsInternal;
+        private readonly Dictionary<string, string> _appBaseUrlsInternal;
+        private readonly Dictionary<string, string> _iccApiBaseUrlsInternal;
+        private readonly Dictionary<string, string> _iccPortalBaseUrlsInternal;
 
         public EndpointConfig(IConfiguration config, string prefix = null) : base(config, prefix)
         {
             _cdnBaseUrls = new Dictionary<string, string>
             {
                 {"dev", "https://localhost:5001"},
-                {"test", "http://corona-dist-content-DEV.covt.mhscibg.nl"},
-                {"acc", "https://acceptatie.coronamelder-dist.nl"},
+                {"test", "https://test.coronamelder-dist.nl"},
+                {"acc", "http://acceptatie.coronamelder-dist.nl"},
                 {"prod", "https://productie.coronamelder-dist.nl"}
             };
 
             _appBaseUrls = new Dictionary<string, string>
             {
                 {"dev", "https://localhost:5002"},
-                {"test", "http://corona-app-mobile-dev.covt.mhscibg.nl"},
-                {"acc", "https://acceptatie.coronamelder-api.nl"},
+                {"test", "https://test.coronamelder-api.nl"},
+                {"acc", "http://acceptatie.coronamelder-api.nl"},
                 {"prod", "https://coronamelder-api.nl"}
             };
 
             _iccApiBaseUrls = new Dictionary<string, string>
             {
                 {"dev", "https://localhost:5003"},
-                {"test", "http://corona-app-services-dev.covt.mhscibg.nl"},
+                {"test", "https://test.coronamelder-portal.nl"},
                 {"acc", "https://acceptatie.coronamelder-portal.nl"},
             };
 
@@ -46,25 +50,75 @@ namespace Core.E2ETests
                 {"test", "https://test.coronamelder-portal.nl"},
                 {"acc", "https://acceptatie.coronamelder-portal.nl"}
             };
+
+            _cdnBaseUrlsInternal = new Dictionary<string, string>
+            {
+                {"dev", "https://localhost:5001"},
+                {"test", "http://corona-dist-content-DEV.covt.mhscibg.nl"},
+                {"acc", "https://acceptatie.coronamelder-dist.nl"},
+                {"prod", "https://productie.coronamelder-dist.nl"}
+            };
+
+            _appBaseUrlsInternal = new Dictionary<string, string>
+            {
+                {"dev", "https://localhost:5002"},
+                {"test", "http://corona-app-mobile-dev.covt.mhscibg.nl"},
+                {"acc", "https://acceptatie.coronamelder-api.nl"},
+                {"prod", "https://coronamelder-api.nl"}
+            };
+
+            _iccApiBaseUrlsInternal = new Dictionary<string, string>
+            {
+                {"dev", "https://localhost:5003"},
+                {"test", "http://corona-app-services-dev.covt.mhscibg.nl"},
+                {"acc", "https://acceptatie.coronamelder-portal.nl"},
+            };
+
+            _iccPortalBaseUrlsInternal = new Dictionary<string, string>
+            {
+                {"dev", "https://localhost:5011"},
+                {"test", "https://test.coronamelder-portal.nl"},
+                {"acc", "https://acceptatie.coronamelder-portal.nl"}
+            };
         }
 
-        public string CdnBaseUrl(string environment)
+        public string CdnBaseUrl(string environment, bool useInternal = false)
         {
+            if (useInternal)
+            {
+                return _cdnBaseUrlsInternal[environment];
+            }
+
             return _cdnBaseUrls[environment];
         }
 
-        public string AppBaseUrl(string environment)
+        public string AppBaseUrl(string environment, bool useInternal = false)
         {
+            if (useInternal)
+            {
+                return _appBaseUrlsInternal[environment];
+            }
+
             return _appBaseUrls[environment];
         }
 
-        public string IccApiBaseUrl(string environment)
+        public string IccApiBaseUrl(string environment, bool useInternal = false)
         {
+            if (useInternal)
+            {
+                return _iccApiBaseUrlsInternal[environment];
+            }
+
             return _iccApiBaseUrls[environment];
         }
 
-        public string IccPortalBaseUrl(string environment)
+        public string IccPortalBaseUrl(string environment, bool useInternal = false)
         {
+            if (useInternal)
+            {
+                return _iccPortalBaseUrlsInternal[environment];
+            }
+
             return _iccPortalBaseUrls[environment];
         }
 
