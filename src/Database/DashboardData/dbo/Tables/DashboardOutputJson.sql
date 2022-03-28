dde4ad5c39951dbbacba0f430bd7975b0fb2a04c
Sabine Kos <sabinekos@gmail.com>
2022-03-28 13:01:49 +0200

    Added PublishedDate column

--- src/Database/DashboardData/dbo/Tables/DashboardOutputJson.sql
+++ src/Database/DashboardData/dbo/Tables/DashboardOutputJson.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[DashboardOutputJson] (
-    [Id]          BIGINT         IDENTITY (1, 1) NOT NULL,
-    [CreatedDate] DATETIME2 (7)  NOT NULL,
-    [JsonData]    NVARCHAR (MAX) NULL,
+    [Id]            BIGINT         IDENTITY (1, 1) NOT NULL,
+    [CreatedDate]   DATETIME2 (7)  NOT NULL,
+    [PublishedDate] DATETIME2 (7)  NULL,
+    [JsonData]      NVARCHAR (MAX) NULL,
     CONSTRAINT [PK_DashboardOutputJson] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
