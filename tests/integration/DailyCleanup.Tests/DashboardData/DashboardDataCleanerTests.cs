9f94e71ecd4ecbce0bd95f4035a137b6924d5a23
Sabine Kos <sabinekos@gmail.com>
2022-05-25 18:10:10 +0200

    Added integration tests (initial commit)

---
+++ tests/integration/DailyCleanup.Tests/DashboardData/DashboardDataCleanerTests.cs
@@ -0,0 +1,67 @@
+// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Abstractions;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.DailyCleanup.Commands.DashboardData;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.DashboardData.Entities;
+using NL.Rijksoverheid.ExposureNotification.BackEnd.DashboardData.EntityFramework;
+using Xunit;
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.DailyCleanup.Tests.DashboardData
+{
+    public abstract class DashboardDataCleanerTests
+    {
+        private readonly DashboardDataDbContext _dbContext;
+
+        protected DashboardDataCleanerTests(DbContextOptions<DashboardDataDbContext> contentDbContextOptions)
+        {
+            _dbContext = new DashboardDataDbContext(contentDbContextOptions ?? throw new ArgumentNullException(nameof(contentDbContextOptions)));
+            _dbContext.Database.EnsureCreated();
+        }
+
+        private void AddTestData(int processed, int unprocessed)
+        {
+            // Add requested amount of processed DashboardData rows
+            for (int i = 0; i < processed; i++)
+            {
+                _dbContext.DashboardInputJson.Add(new DashboardInputJsonEntity
+                {
+                    JsonData = "This is a test",
+                    DownloadedDate = DateTime.UtcNow.AddHours(-1 * i),
+                    ProcessedDate = DateTime.UtcNow,
+                    Hash = "This is not a hash"
+                });
+            }
+
+            // Add requested amount of unprocessed DashboardData rows
+            for (int i = 0; i < unprocessed; i++)
+            {
+                _dbContext.DashboardInputJson.Add(new DashboardInputJsonEntity
+                {
+                    JsonData = "This is a test",
+                    DownloadedDate = DateTime.UtcNow.AddHours(-1 * i),
+                    Hash = "This is not a hash"
+                });
+            }
+
+            _dbContext.SaveChanges();
+        }
+
+        [Fact]
+        public async Task Remove_With_All_Processed_Should_Leave_None()
+        {
+            var command = new RemoveProcessedDashboardDataCommand(_dbContext, new NullLogger<RemoveProcessedDashboardDataCommand>());
+
+            AddTestData(processed: 5, unprocessed: 0);
+
+            var result = (RemoveProcessedDashboardDataResult)await command.ExecuteAsync();
+
+            Assert.Equal(5, result.Removed);
+            Assert.Equal(0, result.Remaining);
+        }
+    }
+}
---
+++ tests/integration/DailyCleanup.Tests/DashboardData/DashboardDataCleanerTestsSqlserver.cs
@@ -0,0 +1,31 @@
+// Copyright 2020 De Staat der Nederlanden, Ministerie van Volksgezondheid, Welzijn en Sport.
+// Licensed under the EUROPEAN UNION PUBLIC LICENCE v. 1.2
+// SPDX-License-Identifier: EUPL-1.2
+
+namespace NL.Rijksoverheid.ExposureNotification.BackEnd.DailyCleanup.Tests.DashboardData
+{
+    [Trait("db", "ss")]
+    public class DashboardDataCleanerTestsSqlserver : DashboardDataCleanerTests
+    {
+        private const string Prefix = nameof(DashboardDataCleanerTests) + "_";
+        private static DbConnection connection;
+
+        public DashboardDataCleanerTestsSqlserver() : base(
+            new DbContextOptionsBuilder<DashboardDataDbContext>().UseSqlServer(CreateSqlDatabase("DD")).Options
+        )
+        { }
+
+        private static DbConnection CreateSqlDatabase(string suffix)
+        {
+            var csb = new SqlConnectionStringBuilder($"Data Source=.;Initial Catalog={Prefix + suffix};Integrated Security=True")
+            {
+                MultipleActiveResultSets = true
+            };
+
+            connection = new SqlConnection(csb.ConnectionString);
+            return connection;
+        }
+
+        public void Dispose() => connection.Dispose();
+    }
+}
